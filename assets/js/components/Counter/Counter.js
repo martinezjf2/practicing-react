82ac1fe112a12201cbcd2f07deaa4544951d76b2
Jeffrey Martinez <martinezjf2@gmail.com>
2023-10-20 14:23:51 -0400

    added inline css styling within the counter component

--- assets/js/components/Counter/Counter.js
+++ assets/js/components/Counter/Counter.js
@@ -3,22 +3,47 @@ import React from 'react';
 export default function Counter() {
 	return (
 		<div id="counter-comp" style={styleCounterComp}>
-			<div className="Number">0</div>
-			<div className="buttons">
-				<div className="minus"> - </div>
-				<div className="plus"> + </div>
+			<div className="Number" style={styleNumber}>
+				0
+			</div>
+			<div className="buttons" style={styleButtons}>
+				<div className="minus" style={styleButton}>
+					{' '}
+					-{' '}
+				</div>
+				<div className="plus" style={styleButton}>
+					{' '}
+					+{' '}
+				</div>
 			</div>
 		</div>
 	);
 }
 
 const styleCounterComp = {
-	width: '100px',
+	width: '100%',
 	maxWidth: '400px',
-	margin: '0 auto'
+	margin: '0 auto',
+	paddingTop: '100px'
 };
 
-const number = {};
-const buttons = {};
-const minus = {};
-const plus = {};
+const styleNumber = {
+	border: '3px solid black',
+	padding: '20px',
+	fontSize: '2rem',
+	fontWeight: '900',
+	textAlign: 'center'
+};
+const styleButton = {
+	width: '50%',
+	border: '3px solid black',
+	padding: '20px',
+	fontSize: '2rem',
+	fontWeight: '900',
+	textAlign: 'center',
+	cursor: 'pointer'
+};
+
+const styleButtons = {
+	display: 'flex'
+};
